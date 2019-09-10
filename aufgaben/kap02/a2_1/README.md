2ef09d77adf83f2c4197bd293aea74f1db711d4f
Khayrat Glende <khayrat.glende@neofonie.de>
2019-09-10 19:41:03 +0200

    excersice 2.1

--- aufgaben/kap02/a2_1/README.md
+++ aufgaben/kap02/a2_1/README.md
@@ -1,7 +1,8 @@
 |Programm|Content|
 |---------|-------|
-|application.cpp       |first try to print color columns from a palette - not so good|
-|pixel_draw_direct.cpp |the correct way to use palette and pixel draw|
+|application.cpp     |first try to print color columns from a palette - not so good|
+|a2_1_pixel_draw.cpp |the correct way to use palette and pixel draw|
+|a2_1_rect.cpp       |renders the palette as a 256x200 rect in the center of the window (640x480)|
 
 # Compile
 
--- aufgaben/kap02/a2_1/a2_1_rect.cpp
+++ aufgaben/kap02/a2_1/a2_1_rect.cpp
@@ -1,6 +1,8 @@
 #include <SDL2/SDL.h>
-
 #include "palette.h"
+
+#define WIN_HEIGHT 480
+#define WIN_WIDTH  640
 
 int main(int argc, char **argv) {
     bool quit = false;
@@ -17,7 +19,7 @@ int main(int argc, char **argv) {
         "Color Palette",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        256, 480, 0);
+        WIN_WIDTH, WIN_HEIGHT, 0);
 
     if (!window) {
         printf("error creating window: %s\n", SDL_GetError());
@@ -41,8 +43,7 @@ int main(int argc, char **argv) {
             renderer,
             SDL_PIXELFORMAT_ARGB8888,
             SDL_TEXTUREACCESS_TARGET,
-            //SDL_TEXTUREACCESS_STATIC,
-            256, 480);
+            WIN_WIDTH, WIN_HEIGHT);
 
     if (!texture) {
         printf("error creating renderer: %s\n", SDL_GetError());
@@ -55,19 +56,24 @@ int main(int argc, char **argv) {
     // render to texture (VRAM)
     SDL_SetRenderTarget(renderer, texture);
 
+    // top left ccordinate when draw-rect position is the center
+    int x = (WIN_WIDTH - 256) / 2;
+    int y = (WIN_HEIGHT - 200) / 2;
+
+    // render to texture (VRAM)
+    SDL_SetRenderTarget(renderer, texture);
+
     // render loop
-    for (int x=0; x<256; x++) {
-      SDL_SetRenderDrawColor(renderer, palette[x].red, palette[x].green, palette[x].blue, palette[x].alpha);
-      for (int y=0; y<480; y++) {
-          SDL_RenderDrawPoint(renderer, x, y);
+    for (int i=0, xx=x; i<256; i++, xx++) {
+      SDL_SetRenderDrawColor(renderer, palette[i].red, palette[i].green, palette[i].blue, palette[i].alpha);
+      for (int yy=y; yy<y+200; yy++) {
+          SDL_RenderDrawPoint(renderer, xx, yy);
       }
     }
 
     SDL_SetRenderTarget(renderer, NULL);
-    // double buffering:
-    // write to back buffer
+    // double buffering: draw to window
     SDL_RenderCopy(renderer, texture, NULL, NULL);
-    // switch to for- and background buffer
     SDL_RenderPresent(renderer);
    
     // wait loop
